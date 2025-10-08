5053dd14098cb5473b6547cc879d3fc0f0bb44b3
SalvadorGriaule <morales.quentin@gmx.fr>
2025-10-08 16:58:39 +0200

    indexage dynamique des md

--- src/components/svelte/Page/BlogPage.svelte
+++ src/components/svelte/Page/BlogPage.svelte
@@ -11,17 +11,17 @@
   onMount(() => {
     const osInstance = OverlayScrollbars(document.body, {
       scrollbars: { theme: "" },
-    });    
+    });
   });
 </script>
 
 <div bind:this={bodySvelte} class="bg-linear-to-br from-slate-600 to-slate-900">
   <HeaderNav />
   <main>
-    {#each posts as post}
-        <div class="text-white text-2xl font-semibold px-2.5">
-            {post.frontmatter.title}
-        </div>
+    {#each posts as post, i}
+      <div class="text-white text-2xl font-semibold px-2.5">
+        <a href="blog/posts/{i + 1}"> {post.frontmatter.title}</a>
+      </div>
     {/each}
   </main>
 </div>
