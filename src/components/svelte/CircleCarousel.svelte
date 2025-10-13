290b4257f174d317d37b237f9d7247fb8921863e
SalvadorGriaule <morales.quentin@gmx.fr>
2025-10-13 14:26:39 +0200

    optimisation CircleCarousel

--- src/components/svelte/CircleCarousel.svelte
+++ src/components/svelte/CircleCarousel.svelte
@@ -1,111 +1,124 @@
 <script lang="ts">
-    import anime from "animejs";
+  import anime from "animejs";
 
-    let {
-        urlArr,
-        currentImg,
-        key = 0,
-    }: { urlArr: string[]; currentImg: number; key: number } = $props();
+  let {
+    urlArr,
+    currentImg,
+    dim,
+    key = 0,
+  }: {
+    urlArr: { src: string; height: number; width: number }[];
+    currentImg: number;
+    dim: number;
+    key?: number;
+  } = $props();
 
-    let sizeSvg = $state({ width: 0, height: 0 });
-    let radial = $state({ offset1: 100, offset2: 100 });
-    let backImg = $state(0);
-    let frontImg = $state(0);
-    let sense: boolean = $state(false);
-    let activ: boolean = $state(false);
-    let wScreen = $state(0);
+  const calcWidth = (dimImg: { height: number; width: number }) => {
+    const rapport = dimImg.width / dimImg.height;
+    return { rapport, newWidth: dimImg.width / (dimImg.width / dim / rapport) };
+  };
 
-    const durationAnim = { minTime: 600, maxTime: 800 };
+  let sizeSvg = $state({ width: 0, height: 0 });
+  let radial = $state({ offset1: 100, offset2: 100 });
+  let backImg = $state(0);
+  let frontImg = $state(0);
+  let sense: boolean = $state(false);
+  let previouse: "back" | "front" | null = $state(null);
+  let activ: boolean = $state(false);
+  let wScreen = $state(0);
 
-    $effect(() => {
-        if (frontImg != currentImg && !activ) {
-            activ = true;
-            sense = frontImg < currentImg;
-            sense ? (frontImg = currentImg) : (backImg = currentImg);
-            radial.offset1 = sense ? 0 : 100;
-            radial.offset2 = sense ? 0 : 100;
-            anime({
-                targets: radial,
-                offset1: {
-                    value: sense ? 100 : 0,
-                    duration: sense
-                        ? durationAnim.maxTime
-                        : durationAnim.minTime,
-                    easing: "easeInSine",
-                },
-                offset2: {
-                    value: sense ? 100 : 0,
-                    duration: sense
-                        ? durationAnim.minTime
-                        : durationAnim.maxTime,
-                    easing: "easeInSine",
-                },
-                complete: function () {
-                    !sense ? (frontImg = currentImg) : (backImg = currentImg);
-                    sense = false;
-                    activ = false;
-                },
-            });
-        }
-    });
+  let { rapport, newWidth } = $derived(calcWidth(urlArr[currentImg]));
+
+  let lastRapport = $state({ rapport, newWidth });
+
+  const durationAnim = { minTime: 600, maxTime: 800 };
+
+  $effect(() => {
+    if (frontImg != currentImg && !activ) {
+      activ = true;
+      sense = frontImg < currentImg;
+      previouse = !sense ? "front" : "back";
+      sense ? (frontImg = currentImg) : (backImg = currentImg);
+      radial.offset1 = sense ? 0 : 100;
+      radial.offset2 = sense ? 0 : 100;
+      anime({
+        targets: radial,
+        offset1: {
+          value: sense ? 100 : 0,
+          duration: sense ? durationAnim.maxTime : durationAnim.minTime,
+          easing: "easeInSine",
+        },
+        offset2: {
+          value: sense ? 100 : 0,
+          duration: sense ? durationAnim.minTime : durationAnim.maxTime,
+          easing: "easeInSine",
+        },
+        complete: function () {
+          !sense ? (frontImg = currentImg) : (backImg = currentImg);
+          sense = false;
+          activ = false;
+          previouse = null;
+          lastRapport = { rapport, newWidth };
+        },
+      });
+    }
+  });
 </script>
 
 <svelte:window bind:innerWidth={wScreen} />
 
 <div
-    class="relative w-[350px] h-[350px] justify-center items-center flex-none rounded-full outline-4 outline-white outline outline-offset-8 bg-white overflow-hidden lg:w-[770px] lg:h-[770px] lg:-mr-36 lg:mt-72 xl:flex"
+  class="relative justify-center items-center flex-none rounded-full outline-4 outline-white outline-offset-8 bg-white overflow-hidden"
+  style:width={dim + "px"}
+  style:height={dim + "px"}
 >
-    <svg class="absolute z-10 left-0 top-0 w-full h-full">
-        <defs>
-            <mask id="circle2{key}">
-                <circle
-                    r={wScreen < 1024
-                        ? sizeSvg.height - 100
-                        : sizeSvg.height - 387} 
-                    cx={sizeSvg.width / 2}
-                    cy={sizeSvg.height / 2}
-                    fill="#ffffff"
-                />
-            </mask>
-        </defs>
-        <image
-            width={wScreen < 1024 ? sizeSvg.width * 1.6 : sizeSvg.width * 1.2}
-            height={wScreen < 1024 ? sizeSvg.width * 1.6 : sizeSvg.height * 1.2}
-            x={wScreen < 1024 ? -90 : -150}
-            y={wScreen < 1024 ? -90 : -150}
-            href={urlArr[backImg]}
-            mask="url(#circle2{key})"
+  <svg class="absolute z-10 left-0 top-0 w-full h-full">
+    <defs>
+      <mask id="circle2{key}">
+        <circle
+          r={sizeSvg.height}
+          cx={sizeSvg.width / 2}
+          cy={sizeSvg.height / 2}
+          fill="#ffffff"
         />
-    </svg>
-    <svg
-        class="absolute z-20 left-0 top-0 w-full h-full"
-        bind:clientWidth={sizeSvg.width}
-        bind:clientHeight={sizeSvg.height}
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <defs>
-            <radialGradient id="grad{key}" cx="50%" r="50%" fx="50%" fy="50%">
-                <stop offset="{radial.offset1}%" stop-color="#ffffff" />
-                <stop offset="{radial.offset2}%" stop-color="#000000" />
-            </radialGradient>
-            <mask id="circle{key}">
-                <circle
-                    r={wScreen < 1024
-                        ? sizeSvg.height - 40
-                        : sizeSvg.height - 387}
-                    cx={sizeSvg.width / 2}
-                    cy={sizeSvg.height / 2}
-                    fill="url(#grad{key})"
-                />
-            </mask>
-        </defs>
-        <image
-            width={wScreen < 1024 ? sizeSvg.width * 1.6 : sizeSvg.width * 1.2}
-            height={wScreen < 1024 ? sizeSvg.width * 1.6 : sizeSvg.height * 1.2}
-            x={wScreen < 1024 ? -90 : -150}
-            y={wScreen < 1024 ? -90 : -150}
-            href={urlArr[frontImg]}
-            mask="url(#circle{key})"
+      </mask>
+    </defs>
+    <image
+      width={previouse == "back" ? lastRapport.newWidth : newWidth}
+      height={dim}
+      x={previouse == "back" ? -lastRapport.newWidth / 8 : -newWidth / 8}
+      y={0}
+      href={urlArr[backImg].src}
+      mask="url(#circle2{key})"
+    />
+  </svg>
+  <svg
+    class="absolute z-20 left-0 top-0 w-full h-full"
+    bind:clientWidth={sizeSvg.width}
+    bind:clientHeight={sizeSvg.height}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <defs>
+      <radialGradient id="grad{key}" cx="50%" r="50%" fx="50%" fy="50%">
+        <stop offset="{radial.offset1}%" stop-color="#ffffff" />
+        <stop offset="{radial.offset2}%" stop-color="#000000" />
+      </radialGradient>
+      <mask id="circle{key}">
+        <circle
+          r={sizeSvg.height}
+          cx={sizeSvg.width / 2}
+          cy={sizeSvg.height / 2}
+          fill="url(#grad{key})"
         />
-    </svg>
+      </mask>
+    </defs>
+    <image
+      width={previouse == "front" ? lastRapport.newWidth : newWidth}
+      height={dim}
+      x={previouse == "front" ? -lastRapport.newWidth / 8 : -newWidth / 8}
+      y={0}
+      href={urlArr[frontImg].src}
+      mask="url(#circle{key})"
+    />
+  </svg>
 </div>
--- src/components/svelte/Page/LibraryPage.svelte
+++ src/components/svelte/Page/LibraryPage.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import HeaderNav from "../HeaderNav.svelte";
   import LogoLib from "../LogoLib.svelte";
+  import CircleCarousel from "../CircleCarousel.svelte";
 
+  // import img
+  import ImgHolder1 from "../../../../public/img/imgHolder.jpg";
+  import ImgHolder2 from "../../../../public/img/imgHolder2.jpg";
+  import ImgHolder3 from "../../../../public/img/imgHolder3.jpg";
+
+  const imgHolder = [ImgHolder1, ImgHolder2, ImgHolder3];
+  let currentImg = $state(0);
   const lib = [{ titre: "Carousel en cercle", logo: ["svelte"] }];
   let selectLib: number | null = $state(null);
   const clickLib = (num: number) => {
-    selectLib = num;
+    selectLib == num ? (selectLib = null) : (selectLib = num);
+  };
+
+  const onclickCircle = (num: number) => {
+    currentImg = num;
   };
 </script>
 
@@ -21,7 +33,7 @@
       </div>
     </section>
     <section class="flex">
-      <div
+      <button
         onclick={() => {
           clickLib(0);
         }}
@@ -31,13 +43,39 @@
         <div class="w-20">
           <LogoLib name={lib[0].logo[0]} mode="no-link" />
         </div>
-      </div>
+      </button>
     </section>
     {#if selectLib != null}
-      <section>
+      <section
+        class="h-fit bg-linear-to-br from-neutral-300/30 to-neutral-500/30 rounded-md m-2 p-2 backdrop-blur-md"
+      >
         <h3 class="text-2xl font-semibold">{lib[selectLib].titre}</h3>
-        <div class="w-20">
-          <LogoLib name={lib[selectLib].logo[0]} mode="no-link" />
+        <div class="flex justify-center items-center">
+          <div>
+            <CircleCarousel urlArr={imgHolder} {currentImg} dim={500} />
+            <div class="flex space-x-2 justify-center">
+              <button
+                onclick={() => onclickCircle(0)}
+                class="p-2 bg-linear-to-l from-blue-300/30 to-blue-400/30 rounded-2xl"
+                >1</button
+              ><button
+                onclick={() => onclickCircle(1)}
+                class="p-2 bg-linear-to-l from-blue-400/30 to-blue-500/30 rounded-2xl"
+                >2</button
+              >
+              <button
+                onclick={() => onclickCircle(2)}
+                class="p-2 bg-linear-to-l from-blue-500/30 to-blue-600/30 rounded-2xl"
+                >3</button
+              >
+            </div>
+          </div>
+        </div>
+        <div class="flex">
+          <p>description</p>
+          <div class="w-20">
+            <LogoLib name={lib[selectLib].logo[0]} mode="no-link" />
+          </div>
         </div>
       </section>
     {/if}
