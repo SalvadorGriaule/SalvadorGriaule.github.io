848f09edae9ee94f0b5daa9f6384238bdb064efc
SalvadorGriaule <morales.quentin@gmx.fr>
2025-12-19 16:50:55 +0100

    add class for ExpPro

--- src/components/svelte/ExpPro.svelte
+++ src/components/svelte/ExpPro.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-
+ 
 </script>
 
 <div>
+    <div class="w-full flex justify-between">
+        <h3>Yzel conseil</h3>
+        <p class="italic">2 stage</p>
+    </div>
+    <div>
 
+    </div>
 </div>
