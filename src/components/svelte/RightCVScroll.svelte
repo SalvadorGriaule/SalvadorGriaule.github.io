47e4daad2066f4ee953ed3cd7e494fa4f2be3a79
SalvadorGriaule <morales.quentin@gmx.fr>
2026-01-14 11:38:45 +0100

    add pdf button

--- src/components/svelte/RightCVScroll.svelte
+++ src/components/svelte/RightCVScroll.svelte
@@ -1,31 +1,43 @@
 <script lang="ts">
   import type { entrerCV } from "./type";
+  import PDFFile from "@assets/Pdf-file.svg"
 
   let { activeRight }: { activeRight: entrerCV | null } = $props();
 </script>
 
 {#if activeRight}
   <div
-    class="p-3 shadow-[inset_0px_0px_25px_-3px_rgba(255,207,152,0.3)] bg-sky-950/10 w-full h-full rounded-2xl"
+    class="p-4 shadow-[inset_0px_0px_25px_-3px_rgba(255,207,152,0.3)] bg-sky-950/10 w-full h-full rounded-2xl"
   >
-    <h3>{activeRight.date}</h3>
-    <h2>{activeRight.titre}</h2>
-    <div>
-      {activeRight.centre.nom}
-      <div class="w-24 h-24 flex justify-center items-center">
-        <img src={activeRight.centre.image} alt={activeRight.centre.nom} />
+    <div class="flex">
+      <div class="mb-4">
+        <h3 class="text-lg">{activeRight.date}</h3>
+        <h2 class="lg:text-xl">{activeRight.titre}</h2>
+        <p>
+          {activeRight.centre.nom}
+        </p>
+        <div class="flex items-center space-x-1">
+          <p>Fiche programme : </p>
+          <a class="text-white font-semibold bg-red-600 p-1 rounded-4xl " href={activeRight.pdf}><div class="flex"><div class="w-8 p-1 bg-white rounded-full"><img src="{PDFFile.src}" alt=""></div><p class="mx-1 font-bold">PDF</p></div> </a>
+        </div>
       </div>
-      <a href={activeRight.pdf}>pdf</a>
+      <div>
+        <div class="w-28 h-28 flex justify-center items-center">
+          <img src={activeRight.centre.image} alt={activeRight.centre.nom} />
+        </div>
+      </div>
     </div>
     <div class="flex">
       <div>
         <div>
-          {activeRight.stage.nom}
+          <h2 class="text-xl">
+            {activeRight.stage.nom}
+          </h2>
           {activeRight.duréeStage}
         </div>
         <div>
           {#each activeRight.taskStage as elem}
-            <p>{elem}</p>
+            <p>- {elem}</p>
           {/each}
         </div>
       </div>
